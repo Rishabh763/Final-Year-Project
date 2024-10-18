4ac9d12d52b62e41384a5efd88e89b37c961f9d2
Rishabh763 <rs0284388@gmail.com>
2024-10-18 19:42:33 +0530

    form update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,7 +22,7 @@ function Home({ handleDiseaseSelect }) {
   return (
     <div classNameName="">
       <div className="content-grid bg-background">
-        <header className="full-width fixed w-full top-0 z-40 bg-background px-4 py-3 shadow-sm md:px-6 md:py-4">
+        <header className="full-width fixed w-full top-0 z-40 bg-background shadow-sm px-6 py-4">
           <div className="container mx-auto flex items-center justify-between">
             <a className="flex items-center" href="#">
               <svg
@@ -40,7 +40,7 @@ function Home({ handleDiseaseSelect }) {
                 <path d="M11 20A7 7 0 0 1 9.8 6.1C15.5 5 17 4.48 19 2c1 2 2 4.18 2 8 0 5.5-4.78 10-10 10Z"></path>
                 <path d="M2 21c0-3 1.85-5.36 5.08-6C9.5 14.52 12 13 13 12"></path>
               </svg>
-              <span className="ml-2 text-lg font-semibold">Mind Matters</span>
+              <span className="ml-2 text-xl font-semibold">Mind Matters</span>
             </a>
             <nav
               className={`${
@@ -74,6 +74,11 @@ function Home({ handleDiseaseSelect }) {
               >
                 Contact
               </NavLink>
+              <Link to="/signup">
+                <button className="inline-flex md:hidden items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2">
+                  Sign Up
+                </button>
+              </Link>
             </nav>
             <div className="flex items-center gap-2">
               {/* <Link to="/login">
@@ -82,12 +87,12 @@ function Home({ handleDiseaseSelect }) {
                 </button>
               </Link> */}
               <Link to="/signup">
-                <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2">
+                <button className="hidden md:inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-10 px-4 py-2">
                   Sign Up
                 </button>
               </Link>
                 <div className="block md:hidden z-50" onClick={toggleMenu}>
-                  <img src="/assets/icon-menu.svg" alt="" />
+                  <img className="h-5 aspect-auto" src="/assets/icon-menu.svg" alt="icon-menu" />
                 </div>
             </div>
           </div>
