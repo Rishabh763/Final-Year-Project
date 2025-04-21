d77db51985aa7924618117cb0c84856f691a707c
Rishabh763 <rs0284388@gmail.com>
2025-04-22 00:24:31 +0530

    Personalized Module Done✅

--- src/components/progressTracker.js
+++ src/components/progressTracker.js
@@ -8,7 +8,7 @@ import {
     ResponsiveContainer,
   } from "recharts";
 
-function progressTracker() {
+function ProgressTracker() {
 
   const barData = [
     { name: "Week 1", value: 60 },
@@ -18,19 +18,19 @@ function progressTracker() {
   ];
   return (
     <div >
-              <h2 className="text-xl font-bold mb-4 text-primary">
-                Progress Tracker
-              </h2>
-              <ResponsiveContainer width="100%" height={300}>
-                <BarChart data={barData}>
-                  <XAxis dataKey="name" />
-                  <YAxis />
-                  <Tooltip />
-                  <Bar dataKey="value" fill="hsl(221.2, 83.2%, 53.3%)" />
-                </BarChart>
-              </ResponsiveContainer>
-            </div>
+      <h2 className="text-xl font-bold mb-4 text-primary">
+        Progress Tracker
+      </h2>
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart data={barData}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Bar dataKey="value" fill="hsl(221.2, 83.2%, 53.3%)" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
   )
 }
 
-export default progressTracker
+export default ProgressTracker
